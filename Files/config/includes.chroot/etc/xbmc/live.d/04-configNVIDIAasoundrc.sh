d8f3288027444a7cc6073dbb07b5a033d82b56b4
Piethein Strengholt <piethein@xbmcfreak.nl>
2012-01-09 11:09:35 +0100

    small unmute fix

--- Files/config/includes.chroot/etc/xbmc/live.d/04-configNVIDIAasoundrc.sh
+++ Files/config/includes.chroot/etc/xbmc/live.d/04-configNVIDIAasoundrc.sh
@@ -224,7 +224,7 @@ do
 	oldifs="$IFS"
 	IFS="
 	"
-	for line in $(/usr/bin/amixer -c $i | grep 'Simple mixer control' | grep 'IEC958' | awk '{print $4,$5}');
+	for line in $(/usr/bin/amixer -c $i | grep 'Simple mixer control' | grep 'IEC958' | awk '{print $4,$6}');
 		do
 			/usr/bin/amixer -q -c $i sset $line unmute &> /dev/null;
 		done;
