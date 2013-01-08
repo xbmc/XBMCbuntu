c80594e25d4cb920ef824ba7b5a7cb0b5810d53c
Luigi Capriotti <l.capriotti@xbmc.org>
2013-01-08 10:07:47 +0100

    Add chroot hooks - fixes #13827

---
+++ copyFiles-hooks.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#      Copyright (C) 2005-2008 Team XBMC
+#      http://www.xbmc.org
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with XBMC; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+
+echo "------------------------------"
+echo "Copying chroot build hooks ..."
+echo "------------------------------"
+
+mkdir -p $WORKPATH/Files/config/hooks &> /dev/null
+
+cp $LIVE_BUILD/share/hooks/004-remove-dbus-machine-id.chroot $WORKPATH/Files/config/hooks
+cp $LIVE_BUILD/share/hooks/006-remove-openssh-server-host-keys.chroot $WORKPATH/Files/config/hooks
+cp $LIVE_BUILD/share/hooks/007-remove-python-py.chroot $WORKPATH/Files/config/hooks
+cp $LIVE_BUILD/share/hooks/008-remove-udev-persistent-rules.chroot $WORKPATH/Files/config/hooks
+cp $LIVE_BUILD/share/hooks/009-remove-apt-sources-lists.chroot $WORKPATH/Files/config/hooks
+
+exit 0
+
