d6b338538907478b89af6f6665093e747b5c12ef
Luigi Capriotti <l.capriotti@xbmc.org>
2013-03-26 14:58:04 +0100

    Provisional - patch to live-build 3.0.1

--- build.sh
+++ build.sh
@@ -96,7 +96,7 @@ if ! which lb > /dev/null ; then
 	if [ ! -d live-build ]; then
 		repoURL="http://live.debian.net/files/current/packages/live-build/orig/"
 		if [ -z "$SDK_USELATESTLIVEBUILD" ]; then
-		    latestPackage="live-build_3.0.2.orig.tar.xz"
+		    latestPackage="live-build_3.0.1.orig.tar.xz"
 		else
 		    latestPackage=$(curl -x "" -s -f $repoURL | grep live-build | tail -1 | grep -o '"live-build_[^"]*.tar..z"' | sed -e "s/\"//g")
 		fi
