a517f00be1419a26ba28005c46ebcf6b47c5fb15
Luigi Capriotti <l.capriotti@xbmc.org>
2014-11-18 17:26:29 +0100

    Fix bad C&P

--- CustomPackages/kodibuntu-initscripts/etc/kodi/setup.d/18-setPolkit.sh
+++ CustomPackages/kodibuntu-initscripts/etc/kodi/setup.d/18-setPolkit.sh
@@ -6,7 +6,7 @@ function polkitfile {
 cat <<EOF
 [Re-enable hibernate by default in upower]
 Identity=unix-user:*
-Action=org.freedesktop.upower.;org.freedesktop.consolekit.system.;org.freedesktop.udisks.;org.freedesktop.login1.
+Action=org.freedesktop.upower.;org.freedesktop.consolekit.system.;org.freedesktop.udisks.;org.freedesktop.login1.*
 ResultActive=yes
 ResultInactive=yes
 ResultAny=yes
